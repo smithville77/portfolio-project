b2d84baaba4fe1a35be9573a997afe6aaf414582
Adam S <volcom_stone664@hotmail.com>
2023-04-08 16:26:31 +1000

    sort alphabetically + keep original id - broke inital render

--- pokemon-project/src/components/Card/Card.js
+++ pokemon-project/src/components/Card/Card.js
@@ -8,37 +8,37 @@ import { CardActionArea } from '@mui/material';
 
 
 function CardDisplay(props) {
-const { name, image, id, chooseHero } = props
+const { name, image, chooseHero, index } = props
 
       
-      function handleClick(name) {
-        chooseHero(name)
-      }
+  function handleClick(name) {
+    chooseHero(name)
+  }
 
   return (
-<Card sx={{ width: 200, height: 250 }}>
-      <CardActionArea>
-        <CardMedia
-          value={name}
-          onClick={() => handleClick(name)}
-          component="img"
-          sx={{height: 170}}
-          image={`${image}`}
-          alt={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {`#${id} `}  
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            info about {name}
-            
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  )
+      <Card sx={{ width: 200, height: 250 }}>
+        <CardActionArea>
+          <CardMedia
+            value={name}
+            onClick={() => handleClick(name)}
+            component="img"
+            sx={{height: 170}}
+            image={`${image}`}
+            alt={name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {`#${index} `}  
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              info about {name}
+              
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+)
 }
 
 export default CardDisplay
--- pokemon-project/src/components/Display/Display.js
+++ pokemon-project/src/components/Display/Display.js
@@ -10,134 +10,88 @@ import { Button, Container } from "@mui/material";
 
 const URL = `http://pokeapi.co/api/v2/pokemon/?limit=151`
 
-function Display() {
 
-  const [ displayList, setDisplayList ] = useState([])
+function Display() {
+  const [displayList, setDisplayList] = useState([]);
+  const [hero, setHero] = useState(null);
+  const [heroURL, setHeroURL] = useState(`https://pokeapi.co/api/v2/pokemon/bulbasaur/`);
 
   useEffect(() => {
     fetch(URL)
-    .then((res) => res.json())
-    .then((data) => setDisplayList(data.results))
-    
+      .then((res) => res.json())
+      .then((data) => setDisplayList(data.results))
+  }, []);
 
-  }, [])
+  useEffect(() => {
+    fetch(heroURL)
+      .then((res) => res.json())
+      .then((data) => setHero(data))
+  }, [heroURL]);
 
- 
 
   const sortAlpha = () => {
-
-    let pokemonIndexList = pokemonList.map((pokemon, index) => {
-      return {
-        ...pokemon,
-        index: index
-      }
-    })
-
-    let sorted = pokemonIndexList.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1
-      } else if (a.name > b.name) {
-        return 1
-      }
-      return 0
-    })
-
-    let sortedResult = sorted.map((data) => ({
-      name: data.name,
-      id: data.index + 1,
-      index: data.index,
-      image: `https://pokeapi.co/api/v2/pokemon/${data.name}/`})
-    )
+    const sortedResult = displayList
+      .map((data, index) => ({
+        name: data.name,
+        id: data.id,
+        index: index,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          index + 1
+        }.png`,
+      }))
+      .sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        } else if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
   
-    
-   return setDisplayList(sortedResult)
+    setDisplayList(sortedResult);
+  };
 
-  
+  const chooseHero = (name, id) => { 
+    setHeroURL(`https://pokeapi.co/api/v2/pokemon/${name}/`)
   }
-
- 
-
-
-
-const pokemonList = displayList.map((data, index) => ({
-      name: data.name,
-      id: index + 1,
-      index: index + 1,
-      // image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.index + 1}.png`,
-      image: `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${index}.svg`
-
-    }))
-
-  // console.log(displayList)
-  // console.log(pokemonList)
-
-
- // set hero areo -----------------------
- const [ hero, setHero ] = useState(null)
- // const heroURL = `https://pokeapi.co/api/v2/pokemon/${displayList}/`
- 
- 
-const [ heroURL, setHeroURL ] = useState(`https://pokeapi.co/api/v2/pokemon/bulbasaur/`)
-
- useEffect(() => {
-   fetch(heroURL)
-   .then((res) => res.json())
-   .then((data) => setHero(data))
- }, [heroURL])
- 
- 
- 
-
- 
- // ------------------------------------------
- 
-    const chooseHero = (name) => { 
-      
-      console.log(name)
-      setHeroURL(`https://pokeapi.co/api/v2/pokemon/${name}/`)
-      
-    }
-
 
   return (
     <>
-    <div style={{marginBottom: "500px"}}>
-    {hero === null ? (
-      <div>Loading...</div>
-    ) : (
-      <Hero 
-      image={hero.sprites.front_shiny}
-      name={hero.species.name}
-      types={hero.types}
-    />
-    )}
-  </div>
+      <div style={{ marginBottom: "500px" }}>
+        {hero === null ? (
+          <div>Loading...</div>
+        ) : (
+            <Hero
+              image={hero.sprites.front_shiny}
+              name={hero.species.name}
+              types={hero.types}
+            />
+          )}
+      </div>
 
-   
+      <Container>
+        <Button onClick={sortAlpha}>Sort alphabetically</Button>
+      </Container>
 
-    <Container>
-      <Button onClick={sortAlpha}>Sort alphabetically</Button>
-    </Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 4, md: 6 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {displayList.map((pokemon, index) => {
+            return (
+              <Grid key={index}>
+                <CardDisplay
+                  chooseHero={(name) => chooseHero(name, pokemon.id)}
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  image={pokemon.image}
+                  index={pokemon.index}
+                />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Box>
 
-     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 4, md: 6 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-           {pokemonList.map((pokemon) => {
-         return <Grid >
-         <CardDisplay  
-            chooseHero={(e) => chooseHero(e)}
-            id={pokemon.id}
-            name={pokemon.name}
-            image={pokemon.image}
-          />
-         </Grid>
-       })}
-          
-      </Grid>
-    </Box>
-      
     </>
-    
   )
 }
-
 export default Display
