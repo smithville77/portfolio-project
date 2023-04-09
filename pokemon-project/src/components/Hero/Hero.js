b86423acd0da6f4661d012cedc85ac82c874293d
Adam S <volcom_stone664@hotmail.com>
2023-04-10 06:58:52 +1000

    changes

--- pokemon-project/src/components/Hero/Hero.js
+++ pokemon-project/src/components/Hero/Hero.js
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material"
 import Image from "mui-image"
-
+import { Link } from "react-router-dom"
+import Button from "@mui/material"
 
 function Hero(props) {
-  const { name, image  } = props
+  const { name, image, id  } = props
   
   
   return (
+    <Link to={`/${id}`} style={{ textDecoration: "none"}}>
     <Container style={{ textAlign: "center", position: "absolute" }}>
       <Image style={{maxHeight: "300px", width: "300px"}} src={`${image}`} />
       <h1>{name}</h1>
@@ -14,7 +16,10 @@ function Hero(props) {
         return <p>{type.type}</p>
       })}
        */}
+       
     </Container>
+    {/* <Button>more Information</Button> */}
+    </Link>
   )
 }
 
