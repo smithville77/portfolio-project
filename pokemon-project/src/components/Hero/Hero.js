3219d14910df0eb19d80a4e74bdb5432cfb278f4
Adam S <volcom_stone664@hotmail.com>
2023-04-17 22:57:28 +1000

    changed Hero inline style to external css

--- pokemon-project/src/components/Hero/Hero.js
+++ pokemon-project/src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import { Container, Button } from "@mui/material"
 import Image from "mui-image"
 import { Link } from "react-router-dom"
+import "./Hero.css"
 
 
 function Hero(props) {
@@ -8,13 +9,12 @@ function Hero(props) {
   let titleCaseName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
   
   return (
-    <Link to={`/pokemon/${id}`} style={{ textDecoration: "none", color: "black"}}>
-    <Container style={{ textAlign: "center", paddingBottom: "40px" }}>
-      <Image style={{maxHeight: "250px", width: "280px"}} src={`${image}`} />
-      <h1>{titleCaseName}</h1>
-       <Button style={{backgroundColor: "#003a70", color: "white"}}>Click to see more</Button>
-    </Container>
-
+    <Link to={`/pokemon/${id}`} className="link">
+      <Container className="container">
+        <Image style={{maxHeight: "250px", width: "300px"}} src={`${image}`} />
+        <h1 className="title">{titleCaseName}</h1>
+        <Button className="button">Click to see more</Button>
+      </Container>
     </Link>
   )
 }
